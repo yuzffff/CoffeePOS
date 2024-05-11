e091021b56a1173780e150600bba94b09af52d58
Yuzffff <user@Users-MacBook-Pro.local>
2024-05-11 16:59:23 +0700

    first commit

---
+++ CoffPOS/admin/config/config.php
@@ -0,0 +1,8 @@
+
+<?php
+    $dbuser="root";
+    $dbpass="root";
+    $host="localhost";
+    $db="rposystem";
+    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ README.md
@@ -0,0 +1,2 @@
+# CoffeePOS
+# CoffeePOS
